805c7f57448f6e229b169c24aa33525c02d7479c
MostafaAbdelfadil <85397234+MostafaAbdelfadil@users.noreply.github.com>
2023-01-31 21:16:36 +0200

    Update Fibunacci_Node.c

--- Question1/Fibunacci_Node.c
+++ Question1/Fibunacci_Node.c
@@ -39,11 +39,14 @@ struct Node* makeValue(int value) {
 
 //Fibunacci Calculation
 int fibonacci(int n) {
-	int* fib = (int*)malloc(sizeof(int) * (n+1));
-	fib[0] = 0;
-	fib[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		fib[i] = fib[i - 1] + fib[i - 2];
+	int* fib = (int*)malloc(sizeof(int) * (n + 1));
+	//fib[0] = 0;
+	//fib[1] = 1;
+	for (int i = 0; i <= n; i++) {
+		if (i <= 1)
+			fib[i] = i;
+		else
+			fib[i] = fib[i - 1] + fib[i - 2];
 	}
 	int a = fib[n];
 	return a;
